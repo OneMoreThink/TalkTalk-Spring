b046af73e260d7ceb037ebbe5e5a10df33d5b459
fine <92whdtjs@gmail.com>
2025-03-28 18:44:37 +0900

    Spring Project init

---
+++ TalkTalk/src/main/java/com/onemorethink/talktalk/TalkTalkApplication.java
@@ -0,0 +1,13 @@
+package com.onemorethink.talktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkTalkApplication.class, args);
+    }
+
+}
---
+++ TalkTalk/src/test/java/com/onemorethink/talktalk/TalkTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemorethink.talktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
